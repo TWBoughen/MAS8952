7329fcab7e5d62aa355b394eb8aba84dd92bf89d
TWBoughen <117275550+TWBoughen@users.noreply.github.com>
2022-11-02 11:47:45 +0000

    Update means.R

--- Assignment1/means.R
+++ Assignment1/means.R
@@ -5,7 +5,7 @@ zero_mean = function(x, ...) {
 
 constant_mean = function(x, c=1, ...) {
   # constant kernel
-  return(numeric(length(x)) + c)
+  return(numeric(length(as.vector(x))) + c)
 }
 
 linear_mean = function(x, a=1, b=1, ...) {
